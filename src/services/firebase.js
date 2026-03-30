7942a046e8a26da6185609d8b17e3c3f5181cd70
nachoquintana1-rgb <nachoquintana1@gmail.com>
2026-03-30 23:28:43 +0200

    Update firebase.js

--- src/services/firebase.js
+++ src/services/firebase.js
@@ -3,12 +3,12 @@ import { getAuth, GoogleAuthProvider } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_AUTH_DOMAIN",
-  projectId: "YOUR_PROJECT_ID",
-  storageBucket: "YOUR_STORAGE_BUCKET",
-  messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
-  appId: "YOUR_APP_ID"
+  apiKey: "AIzaSyBN62iXkuwN3oCVR7vjxCi90HTS2gaC6jI",
+  authDomain: "playandbet.firebaseapp.com",
+  projectId: "playandbet",
+  storageBucket: "playandbet.firebasestorage.app",
+  messagingSenderId: "414960647609",
+  appId: "1:414960647609:web:c58a3ed151f10eb9d9ca68"
 };
 
 let app, auth, db, googleProvider;
